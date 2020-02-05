1180b71e842a3ab5cb6b73afaecab998cc202f31
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2020-02-05 18:07:03 -0300

    Added user add/delete notes functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 export default function App() {
+  const [notes, setNotes] = useState([])
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote ]
+    })
+  }
+
+  function deleteNote(id){
+    setNotes(prevNotes => {
+      return prevNotes.filter((note, index) => {
+        return index !== id
+      })
+    })
+  }
+
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea 
+        onAdd={addNote}
+      />
+      {notes.map((note, index) => {
+          return <Note
+          key={index}
+          id={index} 
+          title={note.title}
+          content={note.content}
+          onDelete={deleteNote} />
+      })}
       <Footer />
     </div>
   );
--- src/components/CreateArea.js
+++ src/components/CreateArea.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 
-export default function CreateArea() {
+export default function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  })
+
+  function handleChange(event){
+    const {name, value} = event.target
+    setNote(prevNote => {
+      return {
+        ...prevNote,
+        [name]: value
+      }
+    })
+  }
+
+  function handleAddNote(event){
+    props.onAdd(note)
+    event.preventDefault()
+    setNote({
+      title: "",
+      content: ""
+    })
+  }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input value={note.title} onChange={handleChange} name="title" placeholder="Title" />
+        <textarea value={note.content} onChange={handleChange} name="content" placeholder="Take a note..." rows="3" />
+        <button onClick={handleAddNote}>Add</button>
       </form>
     </div>
   );
